1eee4fc2f6a25bcfa7a5e7eb83e985ffcb9d2235
Dmytro <Dmytro.Ryshchuk@icloud.com>
2024-11-19 18:27:01 -0600

    Initial Commit

---
+++ My AI Chats/My_AI_ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AI_ChatsApp.swift
+//  My AI Chats
+//
+//  Created by Dmytro Ryshchuk on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct My_AI_ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
